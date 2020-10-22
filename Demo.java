ea44f40e4a5df57fdef867600f76c4edbea37992
anosaryev <anthonynos@outlook.com>
2020-10-21 20:57:14 -0400

    I updated this programme and it works now

--- Demo.java
+++ Demo.java
@@ -16,4 +16,39 @@ public class Demo{
     }
     return str;
   }
+  public static String arrToString(int[] arr){
+    String a = "{";
+    for(int i = 0; i < arr.length; i++){
+      if (i < arr.length - 1) a += arr[i] + ", ";
+      else a += arr[i];
+    }
+    return a + "}";
+  }
+  public static String arrDeepToString(int[][] arr){
+    String a = "{";
+    for(int i = 0; i < arr.length; i ++){
+      if (i < arr.length - 1) a += arrToString(arr[i]) + ", ";
+      else a += arrToString(arr[i]) + "}";
+    }
+    return a;
+  }
+  public static int[][] create2DArray(int rows, int cols, int maxValue){
+    int[][] arr = new int[rows][cols];
+    for (int i = 0; i < rows; i ++){
+      for (int j = 0; j < cols; j ++){
+        arr[i][j] = (int) ((1 + maxValue) * Math.random());
+      }
+    }
+    return arr;
+  }
+  public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue){
+    int[][] arr = new int[rows][];
+    for (int i = 0; i < rows; i ++){
+      arr[i] = new int[(int) ((1 + cols) * Math.random())];
+      for (int j = 0; j < arr[i].length; j ++){
+        arr[i][j] = (int) ((1 + maxValue) * Math.random());
+      }
+    }
+    return arr;
+  }
 }
