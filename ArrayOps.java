08e150aefd8c2c5f605f71657b0e6f91173d5ace
anosaryev <anthonynos@outlook.com>
2020-10-23 12:20:27 -0400

    Completed sum(), largest() and sumRows()

--- ArrayOps.java
+++ ArrayOps.java
@@ -13,4 +13,13 @@ public class ArrayOps {
     }
     return l;
   }
+  public static int[] sumRows(int[][] matrix){
+    int[] k = new int[matrix.length];
+    for (int i = 0; i < matrix.length; i ++){
+      for (int j = 0; j < matrix[i].length; j ++){
+        k[i] = k[i] + matrix[i][j];
+      }
+    }
+    return k;
+  }
 }
