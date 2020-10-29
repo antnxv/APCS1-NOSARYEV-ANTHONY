6653384bc502ad69a3814618fde7e0b89eb7da8c
anosaryev <anthonynos@outlook.com>
2020-10-29 12:04:20 -0400

    Added authenticate, updated some other functions a tad.

--- BankAccount.java
+++ BankAccount.java
@@ -4,18 +4,26 @@ public class BankAccount {
   private String password;
   private double balance;
 
-  public BankAccount(int createAccountID, String createPassword){
-    accountID = createAccountID;
-    password = createPassword;
+  public BankAccount(int accountID, String password){
+    this.accountID = accountID;
+    this.password = password;
     balance = 0.0;
   }
-  public double getBalance() {return balance;}
+  public double getBalance(){
+    return balance;
+  }
 
-  public int getAccountID() {return accountID;}
+  public int getAccountID(){
+    return accountID;
+  }
 
-  public void setPassword(String newPass) {password = newPass;}
+  public void setPassword(String newPass){
+    password = newPass;
+  }
 
-  public String toString() {return accountID + "\t" + balance;}
+  public String toString(){
+    return "#" + accountID + "\t$" + balance;
+  }
 
   public boolean deposit(double amount){
     if (amount >= 0){
@@ -29,5 +37,9 @@ public class BankAccount {
       balance -= amount;
       return true;
     } else return false;
+  }
+
+  private boolean authenticate(String password){
+    return this.password.equals(password);
   }
 }
