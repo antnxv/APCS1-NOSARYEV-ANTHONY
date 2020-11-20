643ae1bec8773a4fd966299428159be2848fa07c
anosaryev <anthonynos@outlook.com>
2020-11-20 13:47:51 -0500

    Created divide()

--- RationalNumber.java
+++ RationalNumber.java
@@ -112,7 +112,9 @@ public class RationalNumber extends RealNumber
   *Return a new RationalNumber that is the this divided by the other
   */
   public RationalNumber divide(RationalNumber other){
-    return null;
+    RationalNumber quotient = new RationalNumber(
+      this.numerator * other.denominator, this.denominator * other.numerator);
+    return quotient;
   }
 
   /**
