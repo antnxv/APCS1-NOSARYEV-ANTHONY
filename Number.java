c314f31c5b03c912a883ab46a2da7440187e3088
anosaryev <anthonynos@outlook.com>
2020-11-25 00:49:27 -0500

    Fixed compareTo

--- Number.java
+++ Number.java
@@ -4,11 +4,13 @@ public abstract class Number{
 
   public int compareTo(Number other){
     if (this.getValue() < other.getValue()) return -1;
-    else return 1;
+    else if (this.getValue() > other.getValue()) return 1;
+    else return 0;
   }
 
   //Return true when the values are within 0.001% of eachother.
   public boolean equals(Number other){
-    return (Math.abs(this.getValue() - other.getValue()) <= 0.00001 * this.getValue());
+    return (Math.abs(this.getValue() - other.getValue())
+                  <= 0.00001 * Math.abs(this.getValue()));
   }
 }
