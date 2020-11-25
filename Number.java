92e636ada2ccbb6d95d427462e6904234b2c5019
anosaryev <anthonynos@outlook.com>
2020-11-25 00:35:27 -0500

    Cleaned up

--- Number.java
+++ Number.java
@@ -6,11 +6,8 @@ public abstract class Number{
     if (this.getValue() < other.getValue()) return -1;
     else return 1;
   }
-  /*
-  *Return true when the values are within 0.001% of eachother.
-  *Special case: if one is exactly zero, the other must be exactly zero.
-  *Modified to work with RationalNumber types.
-  */
+
+  //Return true when the values are within 0.001% of eachother.
   public boolean equals(Number other){
     return (Math.abs(this.getValue() - other.getValue()) <= 0.00001 * this.getValue());
   }
