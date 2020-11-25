3046fb4f869429dc03752c1f1bad0fbc3b3dc0a0
anosaryev <anthonynos@outlook.com>
2020-11-25 00:35:50 -0500

    Cleaned up

--- RealNumber.java
+++ RealNumber.java
@@ -1,4 +1,4 @@
-public class RealNumber{
+public class RealNumber extends Number{
   private double value;
 
   public RealNumber(double v){
@@ -13,48 +13,25 @@ public class RealNumber{
     return ""+getValue();
   }
 
-  /*
-  *Return true when the values are within 0.001% of eachother.
-  *Special case: if one is exactly zero, the other must be exactly zero.
-  *Modified to work with RationalNumber types.
-  */
-  public boolean equals(RealNumber other){
-    return (Math.abs(this.getValue() - other.getValue()) <= 0.00001 * this.getValue());
-  }
-
-  /*
-  *Return a new RealNumber that has the value of:
-  *the sum of this and the other
-  */
+  //Return a new RealNumber equal to this plus the other
   public RealNumber add(RealNumber other){
-     //other can be ANY RealNumber, including a RationalNumber
-     //or other subclasses of RealNumber (that aren't written yet)
      RealNumber sum = new RealNumber(this.getValue() + other.getValue());
      return sum;
   }
 
-  /*
-  *Return a new RealNumber that has the value of:
-  *the product of this and the other
-  */
+  //Return a new RealNumber equal to the this times the other
   public RealNumber multiply(RealNumber other){
     RealNumber product = new RealNumber(this.getValue() * other.getValue());
     return product;
   }
 
-  /*
-  *Return a new RealNumber that has the value of:
-  *this divided by the other
-  */
+  //Return a new RealNumber equal to this divided by the other
   public RealNumber divide(RealNumber other){
     RealNumber quotient = new RealNumber(this.getValue() / other.getValue());
     return quotient;
   }
 
-  /*
-  *Return a new RealNumber that has the value of:
-  *this minus the other
-  */
+  //Return a new RealNumber equal to this minus the other
   public RealNumber subtract(RealNumber other){
     RealNumber difference = new RealNumber(this.getValue() - other.getValue());
     return difference;
