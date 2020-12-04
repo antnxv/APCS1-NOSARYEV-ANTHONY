380b164c33c3c289a7a106220388ea3100895261
anosaryev <anthonynos@outlook.com>
2020-12-04 14:32:05 -0500

    I probably changed something minor

--- IntegerSequence.java
+++ IntegerSequence.java
@@ -1,6 +1,13 @@
 public interface IntegerSequence{
-  boolean hasNext() ;//does the sequence have more elements?
-  int next();         //return the current value in the sequence and advances to the next element
-  int length();     //returns the total length of the sequence
-  void reset();     //start over from the start of the sequence
+  //does the sequence have more elements?
+  boolean hasNext();
+
+  //return the current value in the sequence and advances to the next element
+  int next();
+
+  //returns the total length of the sequence
+  int length();
+
+  //start over from the start of the sequence
+  void reset();
 }
