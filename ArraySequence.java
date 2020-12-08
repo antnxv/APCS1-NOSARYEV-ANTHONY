e03b6d5b70d790c72149da7d4495f249a7ebc22c
anosaryev <anthonynos@outlook.com>
2020-12-08 11:32:23 -0500

    Created constructor, length and hasNext

---
+++ ArraySequence.java
@@ -0,0 +1,30 @@
+import java.util.NoSuchElementException;
+
+public class ArraySequence implements IntegerSequence {
+  private int currentIndex;
+  private int[] data;
+
+  /*Construct the sequence by copying values from the other array into the data array*/
+  public ArraySequence(int [] other){
+    data = new int[other.length];
+    for (int i = 0; i < other.length; i ++){
+      data[i] = other[i];
+    }
+    currentIndex = 0;
+  }
+
+  public void reset(){
+
+  }
+
+  public int length(){
+    return data.length;
+  }
+
+  public boolean hasNext(){
+    return currentIndex < data.length;
+  }
+  public int next(){
+    return 0; // placeholder
+  }
+}
