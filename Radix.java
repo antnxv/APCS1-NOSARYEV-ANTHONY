942c256c079179013cdbcf91a57f9fed5d0f1556
anosaryev <anthonynos@outlook.com>
2021-01-12 15:56:42 -0500

    removed string mechanics from length

--- Radix.java
+++ Radix.java
@@ -4,6 +4,7 @@ public class Radix{
   }
 
   public static int length(int n){
+    if (n == 0) return 0;
     return (int) Math.log10(Math.abs(n)) + 1;
   }
 
@@ -11,16 +12,5 @@ public class Radix{
     for (MyLinkedList i: buckets){
       original.extend(i);
     }
-  }
-
-  public static void main(String[] args){
-    int n = -295192;
-    System.out.println(nth(n, 5));
-    System.out.println(nth(n, 4));
-    System.out.println(nth(n, 3));
-    System.out.println(nth(n, 2));
-    System.out.println(nth(n, 1));
-    System.out.println(nth(n, 0));
-    System.out.println("\n Length of "+n+": "+length(n));
   }
 }
