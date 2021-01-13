153d171d5d2ab1a0ba548a9f8db52ed1eb9cb193
anosaryev <anthonynos@outlook.com>
2021-01-13 00:03:31 -0500

    made radixSort and helper nthneg

--- Radix.java
+++ Radix.java
@@ -3,13 +3,17 @@ public class Radix{
     return Math.abs((n / (int) Math.pow(10, col)) % 10);
   }
 
+  public static int nthneg(int n, int col){
+    return (n / (int) Math.pow(10, col)) % 10;
+  }
+
   public static int length(int n){
     if (n == 0) return 0;
     return (int) Math.log10(Math.abs(n)) + 1;
   }
 
   public static void merge(SortableLinkedList original, SortableLinkedList[] buckets){
-    for (int i = 0; i < 10; i ++){
+    for (int i = 0; i < buckets.length; i ++){
       original.extend(buckets[i]);
     }
   }
@@ -25,12 +29,29 @@ public class Radix{
     while (go == true){
       while (data.size() > 0){
         int num = data.remove(0);
-        System.out.println(nth(num, col));
         buckets[nth(num,col)].add(num);
       }
       if (buckets[0].size() == max) go = false;
       merge(data, buckets);
-      System.out.println(data.toString());
+      col ++;
+    }
+  }
+
+  public static void radixSort(SortableLinkedList data){
+    SortableLinkedList[] buckets = new SortableLinkedList[19];
+    for(int i = 0; i < 19; i ++){
+      buckets[i] = new SortableLinkedList();
+    }
+    int max = data.size();
+    int col = 0;
+    boolean go = true;
+    while (go == true){
+      while (data.size() > 0){
+        int num = data.remove(0);
+        buckets[nthneg(num,col)+9].add(num);
+      }
+      if (buckets[9].size() == max) go = false;
+      merge(data, buckets);
       col ++;
     }
   }
